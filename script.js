98658ed167007f1326e3f9ee1752376ba25b5052
Anya <137091516+anyasauce@users.noreply.github.com>
2024-02-08 14:59:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+const DUPLICATE_THRESHOLD = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    
+    if (noCount < DUPLICATE_THRESHOLD) {
+      shiftNoButtonPosition();
+    } else if (noCount === DUPLICATE_THRESHOLD) {
+      duplicateNoButton();
+    }
+    
+    if (noCount === MAX_IMAGES + DUPLICATE_THRESHOLD) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! i love youu so muchh :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const initialFontSize = parseFloat(window.getComputedStyle(yesButton).getPropertyValue("font-size"));
+  const newFontSize = initialFontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Are you sure?",
+    "Pookie please",
+    "Don't do this to me :(",
+    "You're breaking my heart",
+    "I'm gonna cry...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function shiftNoButtonPosition() {
+  
+}
+
+function duplicateNoButton() {
+  
+}
+
+function changeImage(image) {
+  catImg.src = `cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
